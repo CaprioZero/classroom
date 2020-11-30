d0b01fd096bdf02c2e77cfdd24ef77367e4e04bc
CaprioZero <tom.armc63@gmail.com>
2020-11-30 22:47:25 +0700

    Update database file

--- DATABASE DATA/classroom.sql
+++ DATABASE DATA/classroom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2020 at 10:17 AM
+-- Generation Time: Nov 30, 2020 at 04:46 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -35,15 +35,19 @@ CREATE TABLE `users` (
   `mobilenumber` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `date_time` date NOT NULL,
-  `reset_token` varchar(255) NOT NULL
+  `reset_token` varchar(255) NOT NULL,
+  `user_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `mobilenumber`, `password`, `date_time`, `reset_token`) VALUES
-(1, 'Dương Minh', 'Ngọc', 'nhoxhoakuter2000@gmail.com', '0999999999', '$2y$10$Xv3CN2ZPNB446NS5pxYmhOzkL9y4xb4zkPhu8FswdJap//K9pML46', '2020-11-29', '');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `mobilenumber`, `password`, `date_time`, `reset_token`, `user_type`) VALUES
+(1, 'Dương Minh', 'Ngọc', 'nhoxhoakuter2000@gmail.com', '0999999999', '$2y$10$Xv3CN2ZPNB446NS5pxYmhOzkL9y4xb4zkPhu8FswdJap//K9pML46', '2020-11-29', '', 'admin'),
+(2, 'ngoc', 'ngoc', 'oksdnog@gmail.com', '0123456789', '$2y$10$apREhxdLj3Mu1km0mHtJsOhY2.QIHam0wSYOSSDmAEzjk2f4.FuY2', '2020-11-30', '', 'teacher'),
+(3, 'eg', 'kjs', 'test@gmail.com', '0987654321', '$2y$10$7cLp1OD.ylRaZ4XSAGUtNO2.r6zUaJ6VQB2RpRdi7ISnxDeDU9y36', '2020-11-30', '', 'teacher'),
+(5, 'dgsh', 'wehweh', 'mco09686@cuoly.com', '0888888888', '$2y$10$RmAOjOkQf97swqveWicMbOTCZ7eZOSlHElrgKvrB/MT6FuNItQNKy', '2020-11-30', '', 'student');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
